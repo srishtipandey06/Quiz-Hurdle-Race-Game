7004b55417d0747190f472ee8a096ab2e9f3735c
Srishti Pandey <56189227+srishtipandey06@users.noreply.github.com>
2021-04-15 14:23:04 +0530

    Delete play.py

--- play.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Please choose the number of levels you want to play between (0-3) : "))
-for i in range(a):
- import quiz
- exec(open("main.py").read(), globals())
- exec(open("main1.py").read(), globals())
- break
